0ca455a0bc959ea6fe984b513c041a54ed22ad07
cockmake <2359016829@qq.com>
2026-07-12 04:40:12 +0800

    添加注释

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def print_age(age):
 def cat(file_path):
     with open(file_path, mode="r", encoding="utf-8") as f:
         print(f.read())
-
+# <!todo> 提高访问并发上线
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
