608a544bd6aae4c20bbac5c042e2d56d8863c7be
Qian Yiran <657034419@qq.com>
2026-07-12 04:19:19 +0800

    修改main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
+import pandas
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+def print_age(age):
+    print(f"I'm {age} years old.")
 
 
 # 按装订区域中的绿色按钮以运行脚本。
